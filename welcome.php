2c5454b2d046e1041c5cecd4b8a59048233dde9e
ctwhhhhhh233333 <ctw313903233@outlook.com>
2019-10-08 12:41:08 +0800

    login test

--- welcome.php
+++ welcome.php
@@ -11,10 +11,43 @@ session_start();
 	<meta charset="utf-8">
 	<meta name="keywords" content="" />
 
+	<script src="js/jquery-1.10.2.js"></script>
+
+
+<!-- <style>
+ul
+{
+	list-style-type:none;
+	margin:0;
+	padding:0;
+}
+a:link,a:visited
+{
+	display:block;
+	font-weight:bold;
+	color:#FFFFFF;
+	background-color:#98bf21;
+	width:120px;
+	text-align:center;
+	padding:4px;
+	text-decoration:none;
+	text-transform:uppercase;
+}
+a:hover,a:active
+{
+	background-color:#7A991A;
+}
+</style> -->
+
+
 	<!-- default css files -->
 	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/test.css">
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<!-- <link rel="stylesheet" herf="button_test.css"> -->
 	<!-- default css files -->
 
 	<!--web font-->
@@ -24,102 +57,73 @@ session_start();
 
 
 	<!-- Body -->
-
+</head>
 <body>
-	<!-- banner -->
-	<div class="banner1">
-		<div class="header-top">
-			<div class="container">
-				<div class="header-top-right">
-					<p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:example@example.com">example@hust.edu.cn</a></p>
-					<?php
-					if (isset($_SESSION['username'])) {
-						$name = $_SESSION['username'];
-						if($_SESSION['isTeacher'])
-							 echo "<p>欢迎,$name 老师</p>";
-						else
-							echo "<p>欢迎,$name 同学</p>";
-					}
-					?>
-				</div>
+	<div class="row">
+		<div class="side">
+
+			<p><b><a href="index.php" data-hover="Home">主页</a></b></p>
+			<p><b><a href="syllabus.html">课程表 </a></b></p>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-hover="Pages" data-toggle="dropdown">通知<b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="event.html#school">学校通知</a></li>
+					<li><a href="event.html#class">课堂通知</a></li>
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-hover="Pages" data-toggle="dropdown">提交<b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="submit.html#assignment">提交作业</a></li>
+					<li><a href="submit_exp.html#lab">提交实验</a></li>
+				</ul>
+			</li>
+		</div>
+
+
+
+
+
+		<div class="login_cont">
+		<div class="login_nav">
+			<div class="nav_slider">
+				<a href="#" class="signup focus">注册</a>
+				<a href="#" class="signin">登录</a>
 			</div>
 		</div>
-		<div class="head">
-			<div class="container">
-				<div class="navbar-top">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<div class="navbar-brand logo ">
-							<h1 class="animated wow pulse" data-wow-delay=".5s">
-								<a href="index.php">华中科技大学网络课堂系统</a></h1>
-						</div>
-
-					</div>
-
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav link-effect-4">
-							<li><a href="index.php" data-hover="Home">主页</a> </li>
-							<li><a href="syllabus.html">课程表 </a> </li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-hover="Pages" data-toggle="dropdown">通知<b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="event.html#school">学校通知</a></li>
-									<li><a href="event.html#class">课堂通知</a></li>
-								</ul>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-hover="Pages" data-toggle="dropdown">提交<b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="submit.html#assignment">提交作业</a></li>
-									<li><a href="submit_exp.html#lab">提交实验</a></li>
-								</ul>
-							</li>
-							<!-- <li ><a href="index.html" >退出登录</a> </li> -->
-						</ul>
-					</div><!-- /.navbar-collapse -->
-				</div>
-				<div class="clearfix"></div>
+		<form>
+			<div class="input_signup active">
+				<input class="input" id="user_name" type="text" aria-label="用户名（包含字母／数字／下划线" placeholder="用户名">
+				<div class="hint">请填写符合格式的用户名</div>
+				<input class="input" id="user_email" type="text" aria-label="邮箱" placeholder="邮箱">
+				<div class="hint">请填写邮箱</div>
+				<input class="input" id="phone" type="text" class="account" aria-label="手机号（仅支持中国大陆）" placeholder="手机号（仅支持中国大陆）">
+				<div class="hint">请填写手机号</div>
+				<input class="input" id="password" type="password" aria-label="密码" placeholder="密码（不少于 6 位）">
+				<div class="hint">请填写符合格式的密码</div>
+				<input class="input" id="repassword" type="password" aria-label="密码" placeholder="再次输入密码">
+				<div class="hint">请再次输入密码</div>
+				<input type="submit" id="submit" class="button" name="button" value="注册">
 			</div>
-		</div>
-		<h2>注册或登录</h2>
-	</div>
-	<!-- //banner -->
-	<div class="sideleft">
-		<div class="index">
-			<form action="logic/signup.php" method="post">
-				<p class="astyle">用户名：</p>
-				<input type=text name="name">
-				<p class="astyle">密码：</p>
-				<input type=password name="password">
-				<input type="submit" value="注册" name="submit">
-			</form>
-
-		</div>
-
-	</div>
-	<div class="sideright">
-		<div class="index">
-			<form name="login" action="logic/login.php" method="post">
-				<p class="astyle">用户名：</p>
-				<input type=text name="name">
-				<p class="astyle">密码：</p>
-				<input type=password name="password">
-				<input type="submit" value="登陆" name="submit">
-			</form>
-		</div>
+		</form>
+		<form>
+			<div class="input_signin">
+				<input class="input" id="login_user_name" type="text" aria-label="用户名" placeholder="用户名">
+				<div class="hint">请输入用户名</div>
+				<input class="input" id="login_password" type="password" aria-label="密码" placeholder="密码">
+				<div class="hint">请输入密码</div>
+				<input type="submit" id="button" class="button" name="button" value="登录">
+				<div class="forget">
+					<a href="forget.html">忘记密码？</a>
+				</div>
+			</div>
+		</form>
 	</div>
 
-	<!-- Default-JavaScript-File -->
-	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.js"></script>
-	<!-- //Default-JavaScript-File -->
+	<script type="text/javascript" src="js/login.js"></script>
+	<script type="text/javascript" src="js/form.js"></script>
+	<script type="text/javascript" src="js/config.js"></script>
+	<script type="text/javascript" src="js/login_ajax.js"></script>
 
 
 </body>
