facee463077d3effde451f7fa6abd6ab90c920db
Zayle <zaylezou@gmail.com>
2019-10-09 13:20:55 +0800

    welcome complete

--- logic/signin.php
+++ logic/signin.php
@@ -1,39 +1,37 @@
 <?PHP
-    header("Content-Type: text/html; charset=utf8");
-    if(!isset($_POST["submit"])){
-        exit("无权限调用！");
-    }//检测是否有submit操作 
-    include('connect.php');//链接数据库
-    $name = $_POST['name'];//post获得用户名表单值
-    $passowrd = $_POST['password'];//post获得用户密码单值
+header("Content-Type: text/html; charset=utf8");
+if (!isset($_POST["submit"])) {
+    exit("无权限调用！");
+} //检测是否有submit操作 
+include('connect.php'); //链接数据库
+$name = $_POST['name']; //post获得用户名表单值
+$passowrd = $_POST['password']; //post获得用户密码单值
 
-    if ($name && $passowrd){//如果用户名和密码都不为空
-             $sql = "select * from users where username = '$name' and password='$passowrd'";//检测数据库是否有对应的username和password的sql
-             $result = mysqli_query($con,$sql);//执行sql
-             $rows=mysqli_num_rows($result);//返回一个数值
-             $row=mysqli_fetch_assoc($result);
-             if($rows>0){
-                 echo "<script>alert('登陆成功!')</script>";
-                 header("refresh:0;url=../welcome.php");
-                //    session_start();
-                //    $_SESSION['username']=$name;
-             }else{
-                echo "<script>alert('用户名或密码错误！')</script>";
-                echo "
+if ($name && $passowrd) { //如果用户名和密码都不为空
+    $sql = "select * from users where username = '$name' and password='$passowrd'"; //检测数据库是否有对应的username和password的sql
+    $result = mysqli_query($con, $sql); //执行sql
+    $rows = mysqli_num_rows($result); //返回一个数值
+    $row = mysqli_fetch_assoc($result);
+    if ($rows > 0) {
+        session_start();
+        $_SESSION['username'] = $name;
+        echo "<script>alert('登陆成功!')</script>";
+        header("refresh:0;url=../welcome.php");
+    } else {
+        echo "<script>alert('用户名或密码错误！')</script>";
+        echo "
                     <script>
                             setTimeout(function(){window.location.href='../signinorup.php';},1000);
                     </script>
                 ";
-             }
-             
-
-    }else{//如果用户名或密码有空
-                echo "<script>alert('用户名或密码不能为空！')</script>";
-                echo "
+    }
+} else { //如果用户名或密码有空
+    echo "<script>alert('用户名或密码不能为空！')</script>";
+    echo "
                       <script>
                             setTimeout(function(){window.location.href='../signinorup.php';},1000);
                       </script>";
 
-                        //如果错误使用js 1秒后跳转到登录页面重试;
-    }
-    mysqli_close($con);
+    //如果错误使用js 1秒后跳转到登录页面重试;
+}
+mysqli_close($con);
---
+++ welcome.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<!-- Head -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="application/x-javascript">
+	addEventListener("load", function() {
+		setTimeout(hideURLbar, 0);
+	}, false);
+
+	function hideURLbar() {
+		window.scrollTo(0, 1);
+	}
+</script>
+<meta charset utf="8">
+
+<head>
+	<!--font-awsome-css-->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!--bootstrap-->
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<!--custom css-->
+	<link href="css/style_welcome.css" rel="stylesheet" type="text/css" />
+	<!--component-css-->
+	<script src="travel/js/jquery-2.1.4.min.js"></script>
+	<script src="travel/js/bootstrap.min.js"></script>
+	<!--script-->
+	<script src="travel/js/modernizr.custom.js"></script>
+	<script src="travel/js/bigSlide.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('.menu-link').bigSlide();
+		});
+	</script>
+	<!-- web-fonts -->
+	<link href='http://fonts.useso.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.useso.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<!-- //web-fonts -->
+</head>
+
+<body>
+	<div class="body-back">
+		<div class="masthead pdng-stn1">
+			<div class="phone-box wrap push" id="home">
+				<div class="menu-notify">
+					<div class="Profile-mid">
+						<h5 class="pro-link"><a href="welcome.php">微日记</a></h5>
+					</div>
+					<div class="Profile-right">
+						<?php
+						if (isset($_SESSION['username'])) 
+						{
+							$name = $_SESSION['username'];
+						}
+						else
+						$name = '游客';
+						echo "<a>$name , 你好！</a>";
+						?>
+						<!-- modal -->
+
+						<!-- //modal-two -->
+
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<!-- banner -->
+				<div class="details-grid">
+					<div class="details-shade">
+					</div>
+				</div>
+				<div class="w3agile banner-bottom">
+				<ul>
+				    <li><a href="flight.html" class="hvr-radial-out"><i class="fa fa-upload" aria-hidden="true"></i></a><h6>上传作文</h6></li>
+					<li><a href="train.html" class="hvr-radial-out"><i class="fa fa-star" aria-hidden="true"></i></a><h6>优秀作文</h6></li>
+					<li><a href="bus.html" class="hvr-radial-out"><i class="fa fa-list" aria-hidden="true"></i></a><h6>我的作文</h6></li>
+					<li><a href="hotel.html" class="hvr-radial-out"><i class="fa fa-user" aria-hidden="true"></i></a><h6>修改资料</h6></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+</body>
