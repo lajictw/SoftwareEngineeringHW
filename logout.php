1585544496d1f29e90a99c565da4ec4569983c62
Zayle <zaylezou@gmail.com>
2019-11-15 10:11:42 +0800

    add 4 files

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION['userid']))
+    unset($_SESSION['userid']);
+if(isset($_SESSION['username']))
+    unset($_SESSION['username']);
+header("refresh:0;url=../index.php");
+echo"<script>alert('你已退出登录！')</script>";
+?>
